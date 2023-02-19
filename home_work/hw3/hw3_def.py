df374c7a47e136d749d0ff5b8c174440315beb5d
Dmitry Khilko <dmitrykhilko@mail.ru>
2023-02-20 02:27:02 +0300

    homework # 5. add new functions to hw3_def.py (convert_to_int, replace_char_in_str, add_to_string, changing_position_words, remove_begin_and_end_spaces, add_class, extracting_element_from_list), add function calls to main.py

--- home_work/hw3/hw3_def.py
+++ home_work/hw3/hw3_def.py
@@ -1,13 +1,83 @@
-# Задание 1. Привести к целому типу -1.6, 2.99
+# Задание 1
 def convert_to_int(number: float) -> int:
     """
        Задание 1. Привести к целому типу -1.6, 2.99
-       :param number: float
-       :return: int
+       :param number: исходное число, которое необходимо привести к integer
+       :return: преобразование float в int
        """
     return int(number)
 
 
+# Задание 2
+def replace_char_in_str(string: str, symbol_old: str, symbol_new: str) -> str:
+    """
+    Замена в строке одного символа на другой
+    :param string: исходная текстовая строка
+    :param symbol_old: заменяемый символ
+    :param symbol_new: символ замены
+    :return: функция замены символа в строке
+    """
+    return string.replace(symbol_old, symbol_new)
 
 
+# Задание 3
+def add_to_string(string: str, string_to_be_added: str) -> str:
+    """
+    Добавление в конец строки дополнительной строки
+    :type string_to_be_added: str
+    :param string: исходная строка
+    :param string_to_be_added: добавляемая строка
+    :return: соединение строк
+    """
+    return string + string_to_be_added
 
+
+# Задание 4
+def changing_position_words(string: str) -> str:
+    """
+    Изменение положения слов в строке из 2-х слов
+    :param string: исходная строка
+    :return: вывод строки с измененным порядком слов
+    """
+    return string.split(" ")[1] + " " + string.split(" ")[0]
+
+
+# Задание 5
+def remove_begin_and_end_spaces(string: str) -> str:
+    """
+    Удаление пробелов в начале и конце строки
+    :type string: basestring
+    :param string: строка с начальными и конечными пробелами
+    :return: строка без начальных и конечных пробелов
+    """
+    return string.strip()
+
+
+# Задание 6
+def add_class(name: dict, class_with_letter: str, students: int) -> dict:
+    """
+    Добавление в школу очередного ученического класса
+    :type name: dict
+    :type class_with_letter: str
+    :param name: имя словаря
+    :param class_with_letter: ученический класс
+    :param students: количество учеников в классе
+    :return: вывод словаря с добавленной новой записью
+    """
+    name[class_with_letter] = students  # Добавление в словарь новой записи
+    return name
+
+
+# Задание 7
+def extracting_element_from_list(user_list: list, element_number: int):
+    """
+    Извлечение из списка n-го элемента
+    :param user_list: пользовательский список
+    :param element_number: номер извлекаемого элемента
+    :return: отображение искомого элемента списка
+    """
+    if element_number <= len(user_list):
+        return user_list[element_number]
+    else:
+        return (f"Номер запрашиваемого элемента списка '{element_number}' "
+                f"больше количества элементов списка")
--- home_work/main.py
+++ home_work/main.py
@@ -1,4 +1,101 @@
-from hw3.hw3_def import *
+from home_work.hw3 import (convert_to_int, replace_char_in_str, add_to_string,
+                           changing_position_words,
+                           remove_begin_and_end_spaces, add_class,
+                           extracting_element_from_list)
+
 
 print(convert_to_int(-1.6))
 print(convert_to_int(2.99))
+
+# hw3. Задание 2
+str_old = "www.my_site.com#about"
+s_old = "#"
+s_new = "/"
+print(replace_char_in_str(str_old, s_old, s_new))
+
+# hw3. Задание 3
+string_1 = "stroka"
+string_2 = "ing"
+print(add_to_string(string_1, string_2))
+
+# hw3. Задание 4
+fio_old = "Ivanov Ivan"
+print(changing_position_words(fio_old))
+
+# hw3. Задание 5
+string = " Мама мыла мылом раму "
+print(remove_begin_and_end_spaces(string))
+
+# hw3. Задание 6
+school = {}
+add_class(school, "1а", 21)
+add_class(school, "2а", 20)
+add_class(school, "3а", 25)
+add_class(school, "4а", 23)
+add_class(school, "5а", 22)
+add_class(school, "6а", 21)
+add_class(school, "7а", 24)
+add_class(school, "8а", 27)
+add_class(school, "9а", 22)
+add_class(school, "10а", 25)
+print(f"Школьные классы: {school}")
+
+# hw3. Задание 7
+list_1 = ["стол", "стул", "кровать", "шкаф"]
+print(extracting_element_from_list(list_1, 1))
+
+# hw3. Задание 8
+# hw3. Задание 9
+# hw3. Задание 10
+
+# variables. Задание 1
+# variables. Задание 2
+# variables. Задание 3
+# variables. Задание 4
+# variables. Задание 5
+
+# strings. Задание 1
+# strings. Задание 2
+# strings. Задание 3
+# strings. Задание 4
+
+# lists. Задание 1
+# lists. Задание 2
+# lists. Задание 3
+# lists. Задание 4
+# lists. Задание 5
+# lists. Задание 6
+# lists. Задание 7
+# lists. Задание 8
+
+# boolean_operations. Задание 1
+# boolean_operations. Задание 2
+# boolean_operations. Задание 3
+# boolean_operations. Задание 4
+
+# dictionaries. Задание 1
+# dictionaries. Задание 2
+# dictionaries. Задание 3
+
+# type_conversion. Задание 1
+# type_conversion. Задание 2
+# type_conversion. Задание 3
+# type_conversion. Задание 4
+# type_conversion. Задание 5
+
+# conditions. Задание 1
+# conditions. Задание 2
+# conditions. Задание 3
+# conditions. Задание 4
+# conditions. Задание 5
+
+# loop_for. Задание 1
+# loop_for. Задание 2
+# loop_for. Задание 3
+# loop_for. Задание 4
+# loop_for. Задание 5
+
+# loop_while. Задание 1
+# loop_while. Задание 2
+# loop_while. Задание 3
+# loop_while. Задание 4
